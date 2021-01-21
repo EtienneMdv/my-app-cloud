491db930cde7e2e274eedf3274b20ce30f4d53d9
Etienne Mandeville <mandeville.etienne@gmail.com>
2021-01-21 16:05:31 +0100

    added webcam

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('/front-app-cloud/index.html', { root: "public" });
+});
+
+router.get('/styles.51a1fa8aedc89704a63a.css', function(req, res, next) {
+  res.sendFile('/front-app-cloud/styles.51a1fa8aedc89704a63a.css', { root: "public" });
+});
+
+router.get('/runtime.0e49e2b53282f40c8925.js', function(req, res, next) {
+  res.sendFile('/front-app-cloud/runtime.0e49e2b53282f40c8925.js', { root: "public" });
+});
+
+router.get('/polyfills.8c151b8375e767ff858f.js', function(req, res, next) {
+  res.sendFile('/front-app-cloud/polyfills.8c151b8375e767ff858f.js', { root: "public" });
+});
+
+router.get('/main.a3ef00dcc2a486e2af52.js', function(req, res, next) {
+  res.sendFile('/front-app-cloud/main.a3ef00dcc2a486e2af52.js', { root: "public" });
 });
 
 module.exports = router;
